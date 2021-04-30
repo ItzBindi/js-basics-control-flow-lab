bed4ae04640d6d16c8017b6ae59f7ddde392a3b6
bindi <snair306@gmail.com>
2021-04-29 19:04:39 -0500

    commit tree 123b430020dd9049802ddda45554adb2db405171

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
-}
+  if (value <= 400) {
+    return 'This one is on me!'
+  }else (value > 2000) 
+    return 'I will gladly take your thirty bucks.'
+  }
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
